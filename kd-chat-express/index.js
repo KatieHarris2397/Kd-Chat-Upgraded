6a77ac6b2c773e9dfaadfd0ecc4f291876e0280b
Katie Harris <kharris@katies-mac-mini.mynetworksettings.com>
2025-02-05 19:51:10 -0500

    Update how build folder is served

--- kd-chat-express/index.js
+++ kd-chat-express/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const axios = require('axios');
 const cors = require('cors');
+const path = require('path');
 
 require('dotenv').config()
 
 const app = express();
 app.use(cors());
 app.use(express.json())
-app.use(express.static("build"))
+app.use(express.static(path.join(__dirname, "build")))
 
 const PORT = 3000;
 
